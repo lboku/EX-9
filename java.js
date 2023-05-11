0bcd4f3f407635e03c6a65a683adcedd5a56f135
lboku <129886726+lboku@users.noreply.github.com>
2023-05-11 20:58:47 +0400

    js

---
+++ java.js
@@ -0,0 +1,9 @@
+var strSalary = prompt("შეიყვანეთ თქვენი ხელფასის ოდენობა");
+if (strSalary >=2500){
+    console.log("კაი ხარ");
+}
+if (strSalary < 2500){
+     console.log("უკეთესიც შეიძება")
+     } 
+     else (strSalary <=1500)
+     console.log("რა გინდა ამ ცხოვრებისგან?")
